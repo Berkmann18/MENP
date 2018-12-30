447b9f59e3aa58d239473062e5786c9c75350b5f
Berkmann18 <maxieberkmann@gmail.com>
2018-12-30 17:30:12 +0000

    Fix(Config): Moved a prop around and some rules
    Moved the `name` property such that it won't be ignored by the assignment process and added ESLint
    directives to prevent the false flag to show up on TravisCI.

--- config/config.js
+++ config/config.js
@@ -3,18 +3,20 @@
 let path = require('path');
 
 /* eslint-disable node/no-unpublished-require */
+/* eslint-disable node/no-missing-require */
 let development = require('./env/development'),
   production = require('./env/production');
+/* eslint-enable node/no-missing-require */
 /* eslint-enable node/no-unpublished-require */
 
 let defaults = {
+  name: 'MENP',
   root: path.normalize(`${__dirname}/..`),
   tokenCooldown: 36e5, //1h in ms
   esig: 'Best regards,\nMENP team\n'
 };
 
 module.exports = {
-  name: 'MENP',
   development: Object.assign(development, defaults),
   production: Object.assign(production, defaults)
 }[process.env.NODE_ENV || 'development'];
